5476318b2d9ac5eea1136025f3c07219384ab0a1
DikeH-Ai <onyekachi.okomba@gmail.com>
2025-01-30 06:52:45 -0500

    retrieve basic info using fastapi

---
+++ main.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timezone
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()  # fastapi instance
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# setup root endpoint
+
+
+@app.get("/")
+async def root():
+    # return basic information
+    return {
+        "email": "onyekachi.okomba@gmail.com",
+        "current_datetime": datetime.now(timezone.utc).isoformat(timespec='seconds').replace('+00:00', 'Z'),
+        "github_url": "try"
+    }
